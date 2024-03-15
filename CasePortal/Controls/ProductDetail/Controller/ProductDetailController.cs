caaf3db5404f6b2923b28f8c254ed011c009875f
Odd Erik Grønberg <odd.erik.gronberg@brandsdalgroup.com>
2024-03-15 13:20:17 +0100

    Added ProductDetailModelFactory

--- CasePortal/Controls/ProductDetail/Controller/ProductDetailController.cs
+++ CasePortal/Controls/ProductDetail/Controller/ProductDetailController.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NK.Web.CasePortal.Controls.ProductDetail.Models;
 using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace NK.Web.CasePortal.Controls.ProductDetail
 {
     [Route("ProductDetail")]
     public class ProductDetailController : Controller
     {
+        private readonly IProductDetailModelFactory _productDetailModelFactory;
+        public ProductDetailController(IProductDetailModelFactory productDetailModelFactory)
+        {
+            _productDetailModelFactory = productDetailModelFactory;
+        }
 
         [Route("{productid?}")]
         public IActionResult ProductDetail(string? id)
         {
             if (id == null) return NotFound();
 
-            return View(new ProductDetailViewModel("TestProduct"));
+            if(!int.TryParse(id, out var productId)) return NotFound();
+
+            return View(_productDetailModelFactory.CreateFrom(int.Parse(id)));
         }
     }
 }
---
+++ CasePortal/Controls/ProductDetail/ProductDetailModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using NK.Web.CasePortal.Controls.ProductList.Models;
+using NK.Web.CasePortal.Controls.ProductList;
+using NK.Web.CasePortal.Controls.ProductDetail.Models;
+
+namespace NK.Web.CasePortal.Controls.ProductDetail
+{
+
+    public interface IProductDetailModelFactory
+    {
+        ProductDetailViewModel CreateFrom(int productId);
+    }
+
+    public class ProductDetailModelFactory : IProductDetailModelFactory
+    {
+        private readonly IProductDetailModelFactoryData _productDetailModelFactoryData;
+
+        public ProductDetailModelFactory(IProductDetailModelFactoryData productDetailModelFactoryData)
+        {
+            _productDetailModelFactoryData = productDetailModelFactoryData;
+        }
+
+        public ProductDetailViewModel CreateFrom(int productId)
+        {
+            return new ProductDetailViewModel(_productDetailModelFactoryData.GetProduct(productId));
+
+        }
+
+    }
+}
